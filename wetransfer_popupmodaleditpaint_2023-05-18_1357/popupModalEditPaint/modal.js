9504760773299fdb01efd056e7bb18df76a3a590
Ric Charles Paquibot <ric2thiss@gmail.com>
2023-05-20 16:06:45 +0800

    Changed the CM image to not be dragable

--- wetransfer_popupmodaleditpaint_2023-05-18_1357/popupModalEditPaint/modal.js
+++ wetransfer_popupmodaleditpaint_2023-05-18_1357/popupModalEditPaint/modal.js
@@ -125,7 +125,13 @@ imageLength.addEventListener("wheel", function (e) {
     inputLength.value = currentValue + 1;
   }
 });
+
+// cm image to not be dragable 
   
+const image1 = document.getElementById('image1')
+const image2 = document.getElementById('image2')
+image1.style.pointerEvents = "none";
+image2.style.pointerEvents = "none";
 
 //  The Data that Saved 
 
